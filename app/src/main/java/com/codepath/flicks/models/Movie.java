a11e23bd4897d7bb64d4037a9ee3a58f21213875
ellcor <ecorbus@college.harvard.edu>
2017-06-23 17:12:46 -0700

    All done

--- app/src/main/java/com/codepath/flicks/models/Movie.java
+++ app/src/main/java/com/codepath/flicks/models/Movie.java
@@ -2,19 +2,25 @@ package com.codepath.flicks.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 /**
  * Created by elliecorbus on 6/21/17.
  * Tracks information for the movie we want to display
  */
 
+@Parcel
 public class Movie {
 
     // values from API
-    private String title;
-    private String overview;
-    private String posterPath; // only the path (not full url)
-    private String backdropPath;
+    String title;
+    String overview;
+    String posterPath; // only the path (not full url)
+    String backdropPath;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() { }
 
     // Constructor: initialize from JSON data
     public Movie(JSONObject object) throws JSONException {
@@ -23,6 +29,7 @@ public class Movie {
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
     }
 
     public String getTitle() {
@@ -39,5 +46,9 @@ public class Movie {
 
     public String getBackdropPath() {
         return backdropPath;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
